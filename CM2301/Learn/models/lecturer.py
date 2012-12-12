1a0214e17134134c996ba02f05c5ec3c493aca18
Charlie Mills <millscr1@cf.ac.uk>
2012-12-12 21:57:26 +0000

    /

---
+++ CM2301/Learn/models/lecturer.py
@@ -0,0 +1,7 @@
+from django.db import models
+from user import User
+from lecture import Lecture
+
+class Lecturer(User):
+    profession = models.CharField(max_length=100)
+    lectures = models.ForeignKey(Lecture)
