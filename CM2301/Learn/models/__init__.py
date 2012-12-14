97a17838fc2521d9b6a879678a7f44d7c4e72b30
Charlie Mills <millscr1@cf.ac.uk>
2012-12-14 02:28:17 +0000

    added some server shite

--- CM2301/Learn/models/__init__.py
+++ CM2301/Learn/models/__init__.py
@@ -2,3 +2,4 @@ from user import User
 from admin import Admin
 from student import Student
 from lecture import Lecture
+from queue import Queue
--- CM2301/Learn/models/lecturer.py
+++ CM2301/Learn/models/lecturer.py
@@ -5,3 +5,6 @@ from lecture import Lecture
 class Lecturer(User):
     profession = models.CharField(max_length=100)
     lectures = models.ForeignKey(Lecture)
+    
+    class Meta:
+        app_label = "Learn"
