5e401638312b81c0ea293eb9db4ca2b445d5b27a
Joseph Redfern <joseph@redfern.me>
2013-02-01 17:13:19 +0000

    Static file path… this should be made relative with some jazzy python magic stuff, but meh.

--- CM2301/CM2301/settings.py
+++ CM2301/CM2301/settings.py
@@ -70,6 +70,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    '/Users/josephredfern/Development/Django/CM2301/CM2301/CM2301/static',
 )
 
 # List of finder classes that know how to find static files in
