423ac73b0eed2db074fedf46ecbb3e6c3ad9c80a
Joseph Redfern <joseph@redfern.me>
2013-02-04 13:57:13 +0000

    added bootstrap views

--- CM2301/CM2301/urls.py
+++ CM2301/CM2301/urls.py
@@ -7,7 +7,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'CM2301.views.home', name='home'),
-    # url(r'^CM2301/', include('CM2301.foo.urls')),
+    url(r'^$', include('learn.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
