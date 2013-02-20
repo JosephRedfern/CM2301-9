e9a9d4a8450634bc3133eaf69165f4d088e66e51
Charlie Mills <millsc1@cardiff.ac.uk>
2013-02-20 23:26:38 +0000

    Revert "Update CM2301/learn/models.py"
    This reverts commit 1dc6542105c4c41e7eb7b9145aa86ea62cdd6408.

--- CM2301/learn/models.py
+++ CM2301/learn/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, AbstractUser
 from django.conf import settings
 import uuid
 
