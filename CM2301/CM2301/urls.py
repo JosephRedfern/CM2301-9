bef414240a972a122481ca45ea18e1d4835b660f
Geraint Harries <geraint@geraintharries.co.uk>
2013-02-21 00:14:00 +0000

    MADE SOME MOTHER CHANGES

--- CM2301/CM2301/urls.py
+++ CM2301/CM2301/urls.py
@@ -8,7 +8,7 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'learn.views.home', name='home'),
     #url(r'^$', include('learn.urls')),
-    url(r'^articles/2003/$', 'learn.views.current_datetime', name='current_datetime'),
+    url(r'^datetime/$', 'learn.views.current_datetime', name='current_datetime'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
