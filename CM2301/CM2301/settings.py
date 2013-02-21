8ad40a87024dfc9726d1a1c3c28fa011605058c9
Joseph Redfern <joseph@redfern.me>
2013-02-21 20:04:14 +0000

    users can now log in! great success

--- CM2301/CM2301/settings.py
+++ CM2301/CM2301/settings.py
@@ -161,3 +161,6 @@ LOGGING = {
         },
     }
 }
+
+
+LOGIN_URL = '/login/'
--- CM2301/learn/urls.py
+++ CM2301/learn/urls.py
@@ -5,4 +5,5 @@ urlpatterns = patterns('',
     url(r'^$', 'learn.views.home', name='home'),
     url(r'^videos/$', 'learn.views.videos'),
     url(r'^videos/submit/$', 'learn.views.video_submit'),
+    url(r'^login/$', 'django.contrib.auth.views.login',{'template_name': 'login.html'}),
 )
