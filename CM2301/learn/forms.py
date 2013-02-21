b34a14def45488880766f93634ecf7e505f4472b
Charlie Mills <millsc1@cardiff.ac.uk>
2013-02-21 19:55:50 +0000

    Added datepicker to "valid_from" and "valid_to" fields

--- CM2301/learn/forms.py
+++ CM2301/learn/forms.py
@@ -1,5 +1,6 @@
 from django.forms import ModelForm
 from learn.models import *
+from django.forms import ModelForm, DateInput
 
 class VideoUploadForm(ModelForm):
     class Meta:
@@ -9,3 +10,7 @@ class LectureCreateForm(ModelForm):
     class Meta:
         model = Lecture
         exclude = ('id', 'visible')
+        widgets = {
+            'validFrom': DateInput(attrs={'class': 'datepicker', 'data-date-format': 'dd/mm/yy'}),
+            'validTo': DateInput(attrs={'class': 'datepicker', 'data-date-format': 'dd/mm/yy'}),
+        }
