2465ec15b40324af8ce4d90d525a21dab3d30956
Charlie Mills <millsc1@cardiff.ac.uk>
2013-02-21 00:40:42 +0000

    Added view for video upload

--- CM2301/learn/views.py
+++ CM2301/learn/views.py
@@ -1,4 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def home(request):
     return render(request, 'base_navbar.html')
+
+
+def videos(request):
+    form = VideUploadForm()
+    videos = Video.objects.all()
+    return render(request, 'video_upload.html', {'videos': videos, 'form': form})
+
+def video_submit(request):
+    if request.method == 'POST':
+        form = VideUploadForm(data=request.POST, files=request.FILES)
+        
+        if form.is_valid():
+            form.save()
+            return HttpResponse("MOTHER FUCKER IT UPLOADED")
