b083b7d35507011712724fcbe5bf1b0c709c5b75
Charlie Mills <millsc1@cardiff.ac.uk>
2013-02-21 01:40:21 +0000

    Corrected video upload issues

--- CM2301/learn/views.py
+++ CM2301/learn/views.py
@@ -1,22 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-
+from learn.models import Video
+from learn.forms import *
 
 def home(request):
     return render(request, 'base_navbar.html')
 
+
 def videos(request):
-    form = VideUploadForm()
+    form = VideoUploadForm()
     videos = Video.objects.all()
     return render(request, 'video_upload.html', {'videos': videos, 'form': form})
 
 def video_submit(request):
     if request.method == 'POST':
-        form = VideUploadForm(data=request.POST, files=request.FILES)
+        form = VideoUploadForm(data=request.POST, files=request.FILES)
         
         if form.is_valid():
             form.save()
-            return HttpResponse("MOTHER FUCKER IT UPLOADED YOU PRICK ")
-    
-
+            return HttpResponse("MOTHER FUCKER IT UPLOADED")
