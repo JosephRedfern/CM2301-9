5a9abb766d71064af9f5bb8b2079bcc617a92c98
Charlie Mills <millsc1@cardiff.ac.uk>
2013-02-23 20:02:41 +0000

    Added rudimentary video playing/serving

--- CM2301/learn/urls.py
+++ CM2301/learn/urls.py
@@ -1,11 +1,20 @@
 from django.conf.urls import patterns, include, url
 
+uuid = '[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}'
+
 urlpatterns = patterns('',
     # /
     url(r'^$', 'learn.views.home', name='home'),
-    url(r'^videos/$', 'learn.views.videos'),
+    #url(r'^videos/$', 'learn.views.videos'),
     url(r'^videos/submit/$', 'learn.views.video_submit'),
     url(r'^login/$', 'django.contrib.auth.views.login',{'template_name': 'login.html'}),
+    
+    #Video URL's
+    url(r'^videos/$', 'learn.views.videos'),
+    url(r'^videos/(?P<video_id>%s)/$' % (uuid), 'learn.views.video'),
+    url(r'^videos/(?P<video_id>%s)/serve/(.+)$' % (uuid), 'learn.views.serve_video'),
+    url(r'^videos/create/$', 'learn.views.video_create'),
+    
     #Lecture URL's
     url(r'^lectures/create/$', 'learn.views.lecture_create'),
 )
--- CM2301/learn/views.py
+++ CM2301/learn/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login
+from django.http import Http404
 from learn.models import Video, User
 from learn.forms import *
 
@@ -10,22 +13,48 @@ def home(request):
     values['title'] = 'Home'
     return render(request, 'base_navbar.html', values)
 
-
-def videos(request):
+def video_create(request):
     form = VideoUploadForm()
     videos = Video.objects.all()
     return render(request, 'video_upload.html', {'videos': videos, 'form': form})
 
-def video_submit(request):
+def videos(request):
+    videos = Video.objects.all()
+    return render(request, 'video_list.html', {'videos': videos})
+
+
+def video(request, video_id):
+    print video_id
+    try:
+        video = Video.objects.get(pk=video_id)
+    except Video.DoesNotExist:
+        raise Http404('Video %s does not exist' % (video_id))
+    print video.uploaded_video
+    return render(request, 'video_player.html', {'video': video})
+            
+
+def video_submit(request, video_id):
     if request.method == 'POST':
         form = VideoUploadForm(data=request.POST, files=request.FILES)
         
         if form.is_valid():
             form.save()
             return HttpResponse("MOTHER FUCKER IT UPLOADED")
+        
+def serve_video(request, video_id):
+    video = Video.objects.get(pk=video_id)
+    print video.uploaded_video.url
+    filename = video.uploaded_video.name.split('/')[-1]
+    response = HttpResponse(video.uploaded_video, content_type='video/mp4')
+    print video.uploaded_video.file.size
+    response['Content-length'] = video.uploaded_video.file.size
+    
+
+    return response
+    
 
 def login(request):
-    form = LoginForm()
+    form = AuthenticationForm()
     return render(request, 'login.html', {'form': form})
 
         
