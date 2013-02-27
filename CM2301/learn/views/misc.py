055919fdf52b6118264976aa24f0ca78438982ed
Joseph Redfern <joseph@redfern.me>
2013-02-27 21:37:49 +0000

    Made fields some fields optional, added modules to menu

--- CM2301/learn/views/misc.py
+++ CM2301/learn/views/misc.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-from learn.models import Video, User
+from learn.models import Video, User, Module
 from learn.forms import *
 
 def home(request):
     values = {}
     values['title'] = 'Home'
+    values['modules'] = Module.objects.all()
+    print values['modules']
     return render(request, 'base_navbar.html', values)
