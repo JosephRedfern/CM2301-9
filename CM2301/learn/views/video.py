34252703e3577e18807a16798e0ff9e639fb9281
Charlie Mills <millscr1@cf.ac.uk>
2013-02-27 21:49:27 +0000

    Added streaming http response

--- CM2301/learn/views/video.py
+++ CM2301/learn/views/video.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, StreamingHttpResponse
 from django.contrib.auth.decorators import login_required
 from learn.models import Video
 from learn.forms import *
@@ -39,7 +39,7 @@ def serve(request, video_id):
     video = Video.objects.get(pk=video_id)
     print video.uploaded_video.url
     filename = video.uploaded_video.name.split('/')[-1]
-    response = HttpResponse(video.uploaded_video, content_type='video/mp4')
+    response = StreamingHttpResponse(video.uploaded_video, content_type='video/mp4')
     print video.uploaded_video.file.size
     response['Content-length'] = video.uploaded_video.file.size
     
