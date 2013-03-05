9e5af9dccb70010c6d5feb1754aefde616b60ea5
Joseph Redfern <joseph@redfern.me>
2013-03-05 22:44:12 +0000

    Links are better now, and work... maybe other stuff

--- CM2301/learn/views/lecture.py
+++ CM2301/learn/views/lecture.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from learn.forms import *
-from learn.models import Lecture, Module
+from learn.models import Lecture, Module, Link
 from django.contrib.auth.decorators import login_required
 
 @login_required
@@ -18,6 +18,7 @@ def view(request, lecture_id):
     values['modules'] = Module.objects.all()
     values['title'] = values['lecture'].title
     values['attachments'] = Attachment.objects.filter(object_id=lecture_id)
+    values['links'] = Link.objects.filter(object_id=lecture_id)
 
     values['breadcrumb'] = ('LCARS', values['lecture'].module.title + " ("+values['lecture'].module.module_code+')', values['lecture'].title)
     return render(request, 'lecture.html', values)
