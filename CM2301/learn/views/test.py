11d007e59366e27becc15dc4b8f6995889ee4eed
Geraint Harries <geraint@geraintharries.co.uk>
2013-03-06 21:47:03 +0000

    Individual Test working

--- CM2301/learn/views/test.py
+++ CM2301/learn/views/test.py
@@ -8,7 +8,7 @@ def test(request, test_id):
 	values = {}
 	values['test'] = Test.objects.get(pk=test_id)
 	values['lectures'] = values['test'].lecture.module.lecture_set.all()
-
+	values['questions'] = values['test'].questions.all()
 
 	#Don't quite understand this, have hashed something that looks right, needs checking though
 	values['breadcrumb'] = ("LCARS", "%s (%s)"%(values['test'].title,values['test'].description))
