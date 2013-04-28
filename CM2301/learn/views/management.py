018856c8263da73109f92f44fd5f02e027e981e9
Joseph Redfern <joseph@redfern.me>
2013-04-28 19:46:54 +0100

    start work on management interface

--- CM2301/learn/views/management.py
+++ CM2301/learn/views/management.py
@@ -4,3 +4,6 @@ from django.contrib.auth.views import login
 from django.contrib.auth.decorators import login_required
 from learn.forms import *
 
+def overview(request):
+    values=dict()
+    return render(request, "management_overview.html", values)
